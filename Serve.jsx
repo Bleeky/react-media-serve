8c34d6b95b7bcc2e1015265ca02768e266d1ca63
Quentin Hausser <hausser.quentin@gmail.com>
2018-03-14 15:02:50 +0100

    WIP

--- Serve.jsx
+++ Serve.jsx
@@ -1,25 +1,25 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-import { SquareboardMediaIcon } from 'components';
-import { serveFile } from 'components/medias/MediaManager/actions';
-import loadingPic from 'assets/images/nopic/loadingPic.png';
-import uuid from 'uuid/v1';
-import 'intersection-observer';
+import { SquareboardMediaIcon } from "components";
+import serve from "./serve";
+import loadingPic from "assets/images/nopic/loadingPic.png";
+import uuid from "uuid/v1";
+import "intersection-observer";
 
 class Serve extends Component {
   static propTypes = {
     dbPromise: PropTypes.func.isRequired,
     media: PropTypes.shape(),
-    fetchMediaParams: PropTypes.shape(),
+    getMediaParams: PropTypes.shape(),
     mediaUuid: PropTypes.string,
     src: PropTypes.string,
     showContent: PropTypes.bool,
     hash: PropTypes.string,
     classNames: PropTypes.shape(),
     background: PropTypes.bool,
-    onClick: PropTypes.func,
-  }
+    onClick: PropTypes.func
+  };
 
   static defaultProps = {
     hash: null,
@@ -27,14 +27,14 @@ class Serve extends Component {
     onClick: null,
     background: false,
     showContent: false,
-    classNames: {},
+    classNames: {}
   };
 
   constructor(props) {
     super(props);
     this.state = {
       url: null,
-      uniqueHash: uuid(),
+      uniqueHash: uuid()
     };
 
     this.unmounted = false;
@@ -47,42 +47,56 @@ class Serve extends Component {
   }
 
   componentDidMount() {
-      const element = document.getElementById(this.state.uniqueHash);
-      const io = new IntersectionObserver(
-        async (entry) => {
-          if (this.props.mediaUuid &&
-            (this.props.media.type.includes('image') || (this.props.media.extension.includes('pdf') && this.props.showContent))) {
-              if (!this.state.url && entry[0].isIntersecting) {
-                this.promise = serveFile(this.props.mediaUuid, this.props.fetchMedia, {...this.props.fetchMediaParams});
-                const url = await this.promise.call();
-                if (url && !this.unmounted) {
-                  this.setState({ url });
-                }
-              }
+    const element = document.getElementById(this.state.uniqueHash);
+    const io = new IntersectionObserver(
+      async entry => {
+        if (
+          this.props.mediaUuid &&
+          (this.props.media.type.includes("image") ||
+            (this.props.media.extension.includes("pdf") &&
+              this.props.showContent))
+        ) {
+          if (!this.state.url && entry[0].isIntersecting) {
+            this.promise = serve(
+              {
+                dbPromise: this.props.dbPromise,
+                mediaUuid: this.props.mediaUuid,
+                getMedia: this.props.getMedia
+              },
+              { ...this.props.getMediaParams }
+            );
+            const url = await this.promise.call();
+            if (url && !this.unmounted) {
+              this.setState({ url });
             }
-          else if (this.props.src) {
-            this.setState({url: this.props.src})
           }
-        },
-        {
-          threshold: [0],
-        },
-      );
-      io.observe(element);
+        } else if (this.props.src) {
+          this.setState({ url: this.props.src });
+        }
+      },
+      {
+        threshold: [0]
+      }
+    );
+    io.observe(element);
   }
 
   async componentWillReceiveProps(nextProps) {
     if (nextProps.mediaUuid && nextProps.mediaUuid !== this.props.mediaUuid) {
       this.setState({ url: null });
-      if (nextProps.media.type.includes('image') || (nextProps.media.extension.includes('pdf') && nextProps.showContent)) {
-        this.promise = serveFile(nextProps.mediaUuid, nextProps.fetchMedia, {...nextProps.fetchMediaParams});
+      if (
+        nextProps.media.type.includes("image") ||
+        (nextProps.media.extension.includes("pdf") && nextProps.showContent)
+      ) {
+        this.promise = serve(nextProps.mediaUuid, nextProps.getMedia, {
+          ...nextProps.getMediaParams
+        });
         const url = await this.promise.call();
         if (url && !this.unmounted) {
           this.setState({ url });
         }
-      }
-      else if (this.props.src) {
-        this.setState({url: this.props.src})
+      } else if (this.props.src) {
+        this.setState({ url: this.props.src });
       }
     }
   }
@@ -95,37 +109,51 @@ class Serve extends Component {
   }
 
   render() {
-    if (this.props.media.type.includes('image')) {
+    if (this.props.media.type.includes("image")) {
       if (this.props.background) {
         return (
           <div
             id={this.state.uniqueHash}
             onClick={this.props.onClick}
-            className={`${this.props.classNames.image}${this.state.url ? ' is-loaded' : ''}`}
-            style={{ backgroundImage: `url('${this.state.url ? this.state.url : loadingPic}')` }}
-          />);
+            className={`${this.props.classNames.image}${
+              this.state.url ? " is-loaded" : ""
+            }`}
+            style={{
+              backgroundImage: `url('${
+                this.state.url ? this.state.url : loadingPic
+              }')`
+            }}
+          />
+        );
       }
       return (
         <img
           id={this.state.uniqueHash}
           onClick={this.props.onClick}
-          className={`${this.props.classNames.image}${this.state.url ? ' is-loaded' : ''}`}
+          className={`${this.props.classNames.image}${
+            this.state.url ? " is-loaded" : ""
+          }`}
           alt=""
           src={this.state.url ? this.state.url : loadingPic}
         />
       );
     }
-    if (this.props.showContent && this.props.media.extension.includes('pdf')) {
+    if (this.props.showContent && this.props.media.extension.includes("pdf")) {
       return (
         <div
           id={this.state.uniqueHash}
-          style={{ width: '100%', height: '100%' }}
+          style={{ width: "100%", height: "100%" }}
         >
-          <object type="application/pdf" width="100%" height="100%" data={this.state.url ? this.state.url : loadingPic} />
+          <object
+            type="application/pdf"
+            width="100%"
+            height="100%"
+            data={this.state.url ? this.state.url : loadingPic}
+          />
         </div>
       );
     }
-    return (this.props.mediaRenderer(this.props.media));
+    return this.props.mediaRenderer(this.props.media);
   }
 }
 
--- serve.js
+++ serve.js
@@ -1,44 +1,55 @@
-import request from 'request';
-import Wrapper from '../api';
-import store from 'store';
+import request from "request";
+import Wrapper from "../api";
+import store from "store";
 
-const serveFile = (uuid, fetchMedia, mediaArgs, dbPromise) => {
+const serve = (serveParams, mediaArgs) => {
   let hasCanceled = false;
 
   return {
-    call: () => dbPromise.then(db => db.transaction('medias')
-      .objectStore('medias').get(uuid)).then(
-      async (val) => {
-        if (hasCanceled) {
-          return null;
-        }
-        if (!val) {
-            return fetchMedia(mediaArgs).then((res) => {
-              dbPromise.then((db) => {
-                const tx = db.transaction('images', 'readwrite');
-                tx.objectStore('images').put(response, `${hash}[${size}]`);
-                return tx.complete;
-              });
-              if (hasCanceled) {
-                return null;
-              }
-              if (objURL) {
-                return URL.createObjectURL(response);
-              }
-              return response;
-            }).catch(err => {
-              if (hasCanceled) {
-                return null;
-              }
-              return loadingPic;
-            });
+    call: () =>
+      dbPromise
+        .then(db =>
+          db
+            .transaction("medias")
+            .objectStore("medias")
+            .get(serveParams.mediaUuid)
+        )
+        .then(async val => {
+          if (hasCanceled) {
+            return null;
           }
-        return URL.createObjectURL(val);
-      }),
+          if (!val) {
+            return serveParams
+              .fetchMedia(mediaArgs)
+              .then(res => {
+                dbPromise.then(db => {
+                  const tx = db.transaction("images", "readwrite");
+                  tx
+                    .objectStore("images")
+                    .put(response, `${serveParams.mediaUuid}`);
+                  return tx.complete;
+                });
+                if (hasCanceled) {
+                  return null;
+                }
+                if (objURL) {
+                  return URL.createObjectURL(response);
+                }
+                return response;
+              })
+              .catch(err => {
+                if (hasCanceled) {
+                  return null;
+                }
+                return loadingPic;
+              });
+          }
+          return URL.createObjectURL(val);
+        }),
     cancel() {
       hasCanceled = true;
-    },
+    }
   };
 };
 
-export default serveFile;
+export default serve;
